5237cdf2b2cbf55d7f06868a940c2dd48cab6e23
Guo Liu <liuguomailbox@gmail.com>
2018-09-07 17:09:25 +0800

    add start script

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ ipfs-cluster-service path: /data/ipfs-cluster
 
 ## Current multiaddresses
 
-node 0 (controller):
+node 0 (controller): /ip4/13.251.59.100/tcp/9096/ipfs/QmYh1rWxjXtNHKjnpLewd8Azhc49qT1F1X5tZvqxcWbxpk
 node 1: /ip4/13.229.152.92/tcp/9096/ipfs/QmeqbtjnT5PrpDYaPNFsETfx73d39o4YiKQ6YoBcCx86k3
 node 2: /ip4/13.229.150.236/tcp/9096/ipfs/QmdYEPVJ6Y7NzGLqD1Q7qXpgUa38AjyZMjGEQW8FVB7JyN
 node 3: /ip4/13.229.151.136/tcp/9096/ipfs/QmNmdzdj78ixHXnn796vh7g3eKjgYqWyuL41SNfZdxRsDn
-node 4:
+node 4: /ip4/13.229.151.20/tcp/9096/ipfs/QmS5VAJEGMf4UhtCNDLJnFnLtUonzSZw1QayL8WJaSyBRq
--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# firt set CLUSTER_SECRET in the first node:
-# export CLUSTER_SECRET=$(od -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
-# other nodes must use the same secret:
-# echo $CLUSTER_SECRET
-# current secret: 0352322f72d19b0827bd76923c225199e9c2da84847601fc33803d84894b3b78
-
-set -e
-
-[ -z "$CLUSTER_SECRET" ] && echo "Need to set CLUSTER_SECRET" && exit 1;
-
-echo 'export IPFS_PATH=/data/ipfs' >>~/.bash_profile
-echo 'export IPFS_CLUSTER_PATH=/data/ipfs-cluster' >>~/.bash_profile
-source ~/.bash_profile
-
 # install ipfs daemon
 wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
 tar xvfz go-ipfs_*.tar.gz
@@ -34,20 +20,3 @@ tar xvfz ipfs-cluster-ctl_*.tar.gz
 rm ipfs-cluster-ctl_*.tar.gz
 sudo mv ipfs-cluster-ctl/ipfs-cluster-ctl /usr/local/bin
 rm -rf ipfs-cluster-ctl
-
-# init ipfs
-sudo mkdir -p $IPFS_PATH
-sudo chown ubuntu:ubuntu $IPFS_PATH
-ipfs init -p server
-# ipfs config Datastore.StorageMax 100GB
-# uncomment if you want direct access to the instance's gateway
-# ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-
-# init ipfs-cluster-service
-sudo mkdir -p $IPFS_CLUSTER_PATH
-sudo chown ubuntu:ubuntu $IPFS_CLUSTER_PATH
-ipfs-cluster-service init
-if [ ! -z "$CLUSTER_BOOTSTRAP" ]; then
-  sed -i -e "s;\"bootstrap\": \[\];\"bootstrap\": [\"${CLUSTER_BOOTSTRAP}\"];" "${IPFS_CLUSTER_PATH}/service.json"
-fi
-sed -i -e 's;127\.0\.0\.1/tcp/9095;0.0.0.0/tcp/9095;' "${IPFS_CLUSTER_PATH}/service.json"
